9db921491e96193a5d81c749a058aa20d726f87d
24f1002401 <24f1002401@ds.study.iitm.ac.in>
2026-07-15 15:11:59 +0530

    Delete placement_portal_24f1002401 directory

--- placement_portal_24f1002401/backend/run_hint.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-REM From backend folder:
-REM   python main.py
-REM Celery (optional):
-REM   celery -A tasks.celery_app.celery_app worker --loglevel=info --pool=solo
-REM   celery -A tasks.celery_app.celery_app beat --loglevel=info
-ECHO Run: python main.py
